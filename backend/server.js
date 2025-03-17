c097ae4f2539728706f6e618fdefd1b5ae10fc46
VignayReddy <vignayreddymuduganti@gmail.com>
2025-03-17 16:12:12 +0530

    Solved few errors

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
-
-
-
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
@@ -15,11 +12,9 @@ import analyticsRoutes from "./routes/analytics.route.js";
 
 import { connectDB } from "./lib/db.js";
 
-
 dotenv.config();
 const app= express();
 const PORT = process.env.PORT || 5000;
-
 
 app.use(express.json()); // allows you to parse the body of the request
 app.use(cookieParser());
@@ -29,8 +24,6 @@ app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
 }));
-
-
 app.use("/api/auth",authRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/cart",cartRoutes);
