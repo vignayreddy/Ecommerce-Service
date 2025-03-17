b7a646b663f21d93ebc79e0d5de7a5d343e59331
VignayReddy <vignayreddymuduganti@gmail.com>
2025-03-17 16:56:49 +0530

    ...

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js";
@@ -15,6 +16,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app= express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json()); // allows you to parse the body of the request
 app.use(cookieParser());
@@ -31,6 +33,12 @@ app.use("/api/coupons",couponRoutes);
 app.use("/api/payments",paymentRoutes);
 app.use("/api/analytics",analyticsRoutes);
 
+if(process.env.NODE_ENV='production') {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend/dist/index.html"));
+    })
+}
 
 app.listen(PORT,() => 
 {
